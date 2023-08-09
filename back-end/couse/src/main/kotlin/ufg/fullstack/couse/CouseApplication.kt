4e5ec469236e03b77802fc39ba8e2d023dc65d93
LuizCarlosdeSouzaAlmeida <luizcarlosdesouzalmeida@gmail.com>
2023-08-09 01:41:12 -0300

    init project

---
+++ back-end/couse/src/main/kotlin/ufg/fullstack/couse/CouseApplication.kt
@@ -0,0 +1,11 @@
+package ufg.fullstack.couse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouseApplication
+
+fun main(args: Array<String>) {
+	runApplication<CouseApplication>(*args)
+}
---
+++ back-end/couse/src/test/kotlin/ufg/fullstack/couse/CouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package ufg.fullstack.couse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
